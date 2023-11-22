759df7fb209cf5320fa3bd11648c3335da040991
maopl <451052682@qq.com>
2023-11-22 22:35:09 +0800

    Delete Optimizer/__init__.py

--- Optimizer/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from . import LFL
-from . import VanillaBO
-from . import VizerOptimizer
-from . import MultitaskOptimizer
-from . import RGPEOptimizer
-from . import SAEA
-# from . import TPEOptimizer

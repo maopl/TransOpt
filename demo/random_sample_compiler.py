c1c84d50056f29b83a9ed10d2e74bd5d37a30118
neeetman <anshaohac@gmail.com>
2023-12-22 05:55:18 +0000

    Update for experiments

--- demo/random_sample_compiler.py
+++ demo/random_sample_compiler.py
@@ -37,10 +37,9 @@ def split_into_segments(lst, n):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--samples_num",
+        "samples_num",
         type=int,
         help="Number of samples to be collected for each workload",
-        default=6,
     )
     parser.add_argument(
         "--split_index",

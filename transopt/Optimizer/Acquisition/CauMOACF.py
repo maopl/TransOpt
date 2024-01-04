4dada6b895708a0f2988f1e57cbbde8040801109
neeetman <anshaohac@gmail.com>
2024-01-04 13:32:09 +0000

    tree and ucb

--- transopt/Optimizer/Acquisition/CauMOACF.py
+++ transopt/Optimizer/Acquisition/CauMOACF.py
@@ -14,26 +14,67 @@ from transopt.Optimizer.Acquisition.ACF import AcquisitionBase
 from transopt.utils.hypervolume import calc_hypervolume
 from transopt.utils.pareto import find_pareto_front
 from transopt.utils.Register import acf_register
-
+from transopt.utils.Normalization import normalize
 
 class Tree:
-    def __init__(self, id, data=None):
-        self.id = id
-        self.data = data
-        self.children = []
+    def __init__(self, secondary_nodes, evaluated_points):
+        self.secondary_nodes = secondary_nodes
+        self.evaluated_points = evaluated_points
+        
+        self.attach_evaluated_points_to_nodes()
+        self.cal_ucb_for_all()
 
-    def add_child(self, child):
-        self.children.append(child)
+    def attach_evaluated_points_to_nodes(self):
+        # Gather all centroids
+        secondary_centroids = [node.centroid for node in self.secondary_nodes]
+        primary_nodes = [node for sec_node in self.secondary_nodes for node in sec_node.children]
+        primary_centroids = [node.centroid for node in primary_nodes]
+        
+        # Calculate distances and attach points for secondary nodes
+        distances_to_secondary = cdist(self.evaluated_points, secondary_centroids, metric='euclidean')
+        closest_secondary_idx = np.argmin(distances_to_secondary, axis=1)
+        for idx, sec_idx in enumerate(closest_secondary_idx):
+            self.secondary_nodes[sec_idx].evaluated_points.append(self.evaluated_points[idx])
+        
+        # Calculate distances and attach points for primary nodes
+        distances_to_primary = cdist(self.evaluated_points, primary_centroids, metric='euclidean')
+        closest_primary_idx = np.argmin(distances_to_primary, axis=1)
+        for idx, pri_idx in enumerate(closest_primary_idx):
+            primary_nodes[pri_idx].evaluated_points.append(self.evaluated_points[idx])
+            
+    def cal_ucb_for_all(self):
+        for sec_node in self.secondary_nodes:
+            sec_node.ucb = self.cal_ucb(sec_node.nondominants_2, sec_node.evaluated_points, len(self.evaluated_points))
+            
+            for pri_node in sec_node.children:
+                pri_node.ucb = self.cal_ucb(pri_node.nondominants_3, pri_node.evaluated_points, len(sec_node.evaluated_points))
+    
+    def cal_ucb(self, nondominants, evaluated_points, parent_evaluated_count):
+        n_pf = len(nondominants)
+        n_evaluated = len(evaluated_points)
+        n_evaluated = 0.1 if n_evaluated == 0 else n_evaluated  # Avoid division by zero
+        
+        return n_pf / n_evaluated + 0.1 * np.sqrt(parent_evaluated_count / n_evaluated)
+        
+    def get_candidate(self):
+        # Find the secondary node with the highest UCB
+        sec_node = max(self.secondary_nodes, key=lambda node: node.ucb)
+        # Find the primary node with the highest UCB within the selected secondary node
+        pri_node = max(sec_node.children, key=lambda node: node.ucb)
+        
+        return pri_node
 
 class ClusterTreeNode:
-    def __init__(self, members, nondominants_3, nondominants_2, children=None):
+    def __init__(self, members, centroid, nondominants_3, nondominants_2, children=None):
         self.members = members  # All members of this cluster
-        self.centroid 
+        self.centroid = centroid
         self.nondominants_3 = nondominants_3  # Nondominants based on 3 objectives
         self.nondominants_2 = nondominants_2  # Nondominants based on 2 objectives
         self.children = children if children is not None else []  # Children of this node (subclusters)
+        
         self.evaluated_points = [] # Evaluated points attached to this node
-
+        self.ucb = None  # UCB value for this node    
+    
         
 class NondominatedCluster:
     def __init__(self, dataset, cluster_members, nondominated_indices=set()):
@@ -159,31 +200,33 @@ def clustering_by_nearest_nondominate(data, nondominate_set_indices):
     return clusters
 
 
-def create_cluster_tree(dataset, secondary_clusters):
+def create_cluster_tree(secondary_clusters, evaluated_points):
     # Creating secondary level nodes
     secondary_nodes = []
     for sec_cluster in secondary_clusters:
-        node = ClusterTreeNode(
+        secondary_node = ClusterTreeNode(
             members=sec_cluster.members,
-            nondominants_level_1=set(),  # This will be populated from primary clusters
-            nondominants_level_2=sec_cluster.higher_nondominants,
+            centroid=sec_cluster.centroid,
+            nondominants_3=set(),  # This will be populated from primary clusters
+            nondominants_2=sec_cluster.higher_nondominants,
             children=[]
         )
-
+                
+        # Add primary clusters as children of the secondary cluster
         for primary_cluster in sec_cluster.subclusters:
-            child_node = ClusterTreeNode(
+            primary_node = ClusterTreeNode(
                 members=primary_cluster.members,
-                nondominants_level_1=primary_cluster.nondominants,
-                nondominants_level_2=set(), 
+                centroid=primary_cluster.centroid,
+                nondominants_3=primary_cluster.nondominants,
+                nondominants_2=set(),  # As these are primary clusters, level 2 nondominants don't apply
                 children=None  # Primary clusters are leaf nodes
             )
-            node.children.append(child_node)
-            # Accumulate level 1 nondominants
-            node.nondominants_level_1.update(primary_cluster.nondominants)
+            secondary_node.children.append(primary_node)
+            secondary_node.nondominants_3.update(primary_cluster.nondominants)  # Accumulate level 1 nondominants
 
-        secondary_nodes.append(node)
-        
-    return secondary_nodes
+        secondary_nodes.append(secondary_node)
+
+    return Tree(secondary_nodes, evaluated_points)
         
         
 @acf_register("CauMOACF")
@@ -222,15 +265,12 @@ class CauMOACF:
 
         _, pareto_second_index = find_pareto_front(mean[:, 1:], return_index=True)
         higher_clusters = secondary_level_clustering(mean, clusters, pareto_second_index)
-        
-        create_cluster_tree(mean, higher_clusters)
-        root = Tree(id=0)
-        
-        [root.add_child(Tree(k)) for k, c in enumerate(higher_clusters)]
-        for child in root.children:
-            [child.add_child(Tree(k)) for k in relation[child.id].children]
 
-        return
+        evaluated_points = normalize(self.model._Y.T)
+        root = create_cluster_tree(higher_clusters, evaluated_points)
+
+        candidate = root.get_candidate().nondominants_3[0]
+        return candidate
 
 
 

56380422ab41baa3986269cd4284f825c0ba67e1
maopl <451052682@qq.com>
2024-08-09 22:48:42 +0800

    update

--- transopt/benchmark/HPO/__init__.py
+++ transopt/benchmark/HPO/__init__.py
@@ -2,3 +2,6 @@ from transopt.benchmark.HPO.HPORes18 import HPOResNet
 from transopt.benchmark.HPO.HPOSVM import SupportVectorMachine
 from transopt.benchmark.HPO.HPOXGBoost import XGBoostBenchmark
 from transopt.benchmark.HPO.HPOCNN import HPOCNN
+
+
+from transopt.benchmark.HPOOOD.hpoood import ERMOOD
